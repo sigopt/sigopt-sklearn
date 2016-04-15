e4f4e3c13c3b4c65c3bf16952a03ca16cecb56b4
Ian Dewancker <ian@sigopt.com>
2016-04-15 14:30:09 -0700

    adding setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from sigopt_sklearn.version import VERSION
+
+# keep this in sync with requirements.txt
+install_requires=[
+   'sigopt',
+   'numpy',
+   'sklearn']
+
+setup(
+  name='sigopt_sklearn',
+  version=VERSION,
+  description='SigOpt + scikit-learn Integrations',
+  author='SigOpt',
+  author_email='support@sigopt.com',
+  url='https://sigopt.com/',
+  packages=['sigopt_sklearn'],
+  install_requires=install_requires,
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ]
+)
