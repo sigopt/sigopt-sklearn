5253067c299be557983bb2f8ef86bc46ce98447f
Samuel Ainsworth <sam@sigopt.com>
2016-12-15 11:58:34 -0800

    lint

--- sigopt_sklearn/search.py
+++ sigopt_sklearn/search.py
@@ -412,16 +412,13 @@ class SigOptSearchCV(BaseSearchCV):
     return self
 
   def fit(self, X, y=None):
-    """
-    Run fit on the estimator with parameters chosen sequentially by SigOpt.
+    """Run fit on the estimator with parameters chosen sequentially by SigOpt.
 
     Parameters
     ----------
     X : array-like, shape = [n_samples, n_features]
-        Training vector, where n_samples in the number of samples and
-        n_features is the number of features.
+        Training vector, where n_samples in the number of samples and n_features is the number of features.
     y : array-like, shape = [n_samples] or [n_samples, n_output], optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
+        Target relative to X for classification or regression; None for unsupervised learning.
     """
     return self._fit(X, y)
